59dd4b9bed7a019ce09304fbc08a90032d6ce97d
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-08-06 21:45:56 +0530

    add some more specific routes & some changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,19 +5,19 @@ import {
   logout,
   registerUser,
   resetPassword,
+  getMyDetails,
+  changePassword,
+  getAllUsers,
+  getUser,
+  updateUser,
 } from "../controllers/userController.js";
 import "../controllers/authGoogle.js";
 import { googleCallback } from "../controllers/authGoogle.js";
 import passport from "passport";
 import User from "../models/userModel.js";
-const router = express.Router();
+import { isAuthenticated, authorisedRoles } from "../middleware/auth.js";
 
-router.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "home route",
-  });
-});
+const router = express.Router();
 
 router.post("/register", registerUser);
 
@@ -25,9 +25,19 @@ router.post("/login", loginUser);
 
 router.get("/logout", logout);
 
+router.get('/me/:id', getMyDetails);
+
 router.post("/forgotPassword", forgotPassword);
 
 router.put("/forgotPassword:token", resetPassword);
+
+router.put("/changePassword", isAuthenticated, changePassword);
+
+router.get('/admin/allUsers', isAuthenticated, authorisedRoles('admin'), getAllUsers);
+
+router.get('/user/:id', getUser);
+
+router.put("/user/update/:id", updateUser);
 
 router.get(
   "/auth/google",
