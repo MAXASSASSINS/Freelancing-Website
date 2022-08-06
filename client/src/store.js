e71a7c1a4eeb014e5ce28df015ce9896ca2bec81
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-08-06 21:28:46 +0530

    add redux store

---
+++ client/src/store.js
@@ -0,0 +1,28 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { userGigsDetailsReducer, gigReducer, gigDetailReducer } from './reducers/gigReducer.js';
+import { dimBackgroundReducer } from './reducers/dimBackgroundReducer.js';
+import { gigUserReducer, userReducer } from './reducers/userReducers.js';
+
+const reducer = combineReducers({
+    gigs: gigReducer,
+    dimBackground: dimBackgroundReducer,
+    user: userReducer,
+    userGigs: userGigsDetailsReducer,
+    gigUser: gigUserReducer,
+    gigDetail: gigDetailReducer,
+})
+
+let initialsState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialsState,
+    composeWithDevTools(applyMiddleware(...middleware))
+    // applyMiddleware(...middleware)
+);
+
+export default store;
