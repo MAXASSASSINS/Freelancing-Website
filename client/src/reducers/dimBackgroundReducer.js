c259a43cf01f36539af9be1f78f0a710b3560115
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-08-06 21:37:16 +0530

    add dim background contants and reducer

---
+++ client/src/reducers/dimBackgroundReducer.js
@@ -0,0 +1,14 @@
+import { SHOW_DIM_BACKGROUND, HIDE_DIM_BACKGROUND} from "../constants/dimBackgroundConstants";
+
+export const dimBackgroundReducer = ((state = false, action) => {
+    switch (action.type) {
+        case SHOW_DIM_BACKGROUND:
+            state = true;
+            return true;
+        case HIDE_DIM_BACKGROUND:
+            state = false;
+            return false;
+        default:
+            return state;
+    }
+})
