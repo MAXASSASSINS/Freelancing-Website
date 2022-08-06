bfac027c9f12473d5a3c8a9ecd5cb0b40d0f16bf
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-08-06 21:26:55 +0530

    add react-router-dom and dimbackground

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import React, { Fragment, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css';
+import "./app.css"
+import { Footer } from './component/Footer/Footer';
+import { Header } from './component/Header/Header';
+import { Sidebar } from './component/Sidebar/Sidebar';
+import { Home } from './component/Home/Home';
+import { Routes, Route, useParams } from 'react-router-dom'
+import { UserDetail } from './component/UserDetail/UserDetail';
+import { GigDetail } from './component/GigDetail.js/GigDetail';
 const App = () => {
-  return (
-    <div>
-        <h1>hello, MAXASSASSINS</h1>
-    </div>
-  );
+	let { id } = useParams();
+
+	const dimBackground = useSelector(state => state.dimBackground);
+	let height = document.documentElement.offsetHeight;
+	let width = document.documentElement.offsetWidth;
+	const pageheight = window.innerHeight;
+	height = Math.max(pageheight, height);
+
+	const { loading, error, gigs, gigsCount } = useSelector(state => state.gigs);
+
+	return (
+		<Fragment>
+			<Header></Header>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				{/* {
+						gigs && gigs.map(gig => (
+							<Sidebar gig={gig} key={gig._id} />
+						))
+					}
+					<div style={{ height: height - (width > 600 ? 81 : 143) }} className={'search-bar-dim-background ' + (dimBackground ? "visible" : null)}></div> */}
+				<Route exact path='/user/:id' element={<UserDetail />} />
+				<Route exact path='/gig/details/:id' element={<GigDetail />} />
+			</Routes>
+			<Footer />
+		</Fragment>
+	);
 }
 
 export default App;
