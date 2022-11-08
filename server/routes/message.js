61d7ca00e17c30231992bad3592a5dc6ba8bd80f
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-08 19:51:09 +0530

    add isAuthenticated to each route & multer

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,14 +1,37 @@
 import express from 'express'
 import { authorisedRoles, isAuthenticated } from '../middleware/auth.js';
-import { addMessage, getAllMessagesBetweenTwoUsers, getAllMessagesForCurrentUser, getLastMessageBetweenTwoUser, getListOfAllInboxClients } from '../controllers/messageController.js';
+import { addMessage, getAllMessagesBetweenTwoUsers, getAllMessagesForCurrentUser, getLastMessageBetweenTwoUser, getListOfAllInboxClients, sendFileUpload, updateAllMessages } from '../controllers/messageController.js';
 const router = express.Router();
 
-router.post('/add/message', addMessage);
-router.post('/get/all/messages/between/two/users', getAllMessagesBetweenTwoUsers);
-router.get('/get/all/messages/for/current/user', getAllMessagesForCurrentUser);
-router.get('/list/of/all/inbox/clients/for/current/user',isAuthenticated, getListOfAllInboxClients);
-router.post('/get/last/message/between/two/user', getLastMessageBetweenTwoUser);
+import multer from "multer";
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '--' + file.originalname)
+    }
+})
+
+export const upload = multer({ storage: storage }).single("fileUpload");
+// export const uploads = multer({ storage: storage }).array("fileUpload");
+
+router.post('/add/message', isAuthenticated, addMessage);
+router.post('/get/all/messages/between/two/users', isAuthenticated, getAllMessagesBetweenTwoUsers);
+router.get('/get/all/messages/for/current/user', isAuthenticated, getAllMessagesForCurrentUser);
+router.get('/list/of/all/inbox/clients/for/current/user', isAuthenticated, getListOfAllInboxClients);
+router.post('/get/last/message/between/two/user', isAuthenticated, getLastMessageBetweenTwoUser);
+router.post('/send/files', isAuthenticated, upload, sendFileUpload);
+
+
+router.post('/add/file',isAuthenticated, upload, (req, res)=>{
+    console.log(req.file);
+
+    res.send("file uploaded successfully");
+});
+
+router.get('/update/all/messages', updateAllMessages);
 
 export default router;
 
