09be9f67b430bb66cfe6e76ec72b0df6e7763721
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-08 19:49:25 +0530

    remove taking userId from params to req.user now

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,7 @@ import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
 import cloudinary from 'cloudinary'
 import bcrypt from 'bcryptjs'
-import { log } from "console";
+
 // Register our user
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
@@ -148,10 +148,10 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
 
 // Get my details
 export const getMyDetails = catchAsyncErrors(async (req, res, next) => {
-  // const userId = await req.user.id;
+  const userId = await req.user.id;
   // const userId = "62c4882c0648ff3db722b3da";
-  // const user = await User.findById(userId);
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(userId);
+  // const user = await User.findById(req.params.id);
 
   res.status(200).json({
     success: true,
