86663593533571929736a01773d862c198aeb963
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-08 19:27:57 +0530

    add reducer for load user

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -1,20 +1,33 @@
-import { USER_REQUEST, USER_SUCCESS, USER_FAIL, GIG_USER_FAIL, GIG_USER_REQUEST, GIG_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS } from "../constants/userConstants";
+import {
+    USER_REQUEST,
+    USER_SUCCESS,
+    USER_FAIL,
+    GIG_USER_FAIL,
+    GIG_USER_REQUEST,
+    GIG_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER_REQUEST,
+    LOGIN_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS
+} from "../constants/userConstants";
 
-export const userReducer = ((state = null, action) => {
-    switch (action.type) {
-        case USER_REQUEST:
-            return null;
-        case USER_SUCCESS:
-            return action.payload.user;
-        case USER_FAIL:
-            return {
-                loading: false,
-                error: action.payload
-            };
-        default:
-            return state;
-    }
-});
+// export const userReducer = ((state = null, action) => {
+//     switch (action.type) {
+//         case USER_REQUEST:
+//             return null;
+//         case USER_SUCCESS:
+//             return action.payload.user;
+//         case USER_FAIL:
+//             return {
+//                 loading: false,
+//                 error: action.payload
+//             };
+//         default:
+//             return state;
+//     }
+// });
 
 export const gigUserReducer = ((state = null, action) => {
     switch (action.type) {
@@ -33,7 +46,7 @@ export const gigUserReducer = ((state = null, action) => {
 });
 
 const LOGIN_USER_INITIAL_STATE = {
-    isAutheticated: false,
+    isAuthenticated: false,
     loading: false,
     user: JSON.parse(localStorage.getItem("loggedInUser")) || null,
     error: null,
@@ -45,20 +58,20 @@ export const loginUserReducer = ((state = LOGIN_USER_INITIAL_STATE, action) => {
             return {
                 user: null,
                 loading: true,
-                isAutheticated: false,
+                isAuthenticated: false,
                 error: null
             };
         case LOGIN_USER_SUCCESS:
             return {
                 user: action.payload,
                 loading: false,
-                isAutheticated: true,
+                isAuthenticated: true,
                 error: null,
             };
         case LOGIN_USER_FAIL:
             return {
                 loading: false,
-                isAutheticated: false,
+                isAuthenticated: false,
                 user: null,
                 error: action.payload
             };
@@ -66,3 +79,45 @@ export const loginUserReducer = ((state = LOGIN_USER_INITIAL_STATE, action) => {
             return state;
     }
 });
+
+
+export const userReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_REQUEST:
+        case LOAD_USER_REQUEST:
+            return {
+                loading: true,
+                isAuthenticated: false
+            }
+        case USER_SUCCESS:
+        case LOAD_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isAuthenticated: true,
+                user: action.payload
+            }
+
+        case USER_FAIL:
+            return {
+                ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+        case LOAD_USER_FAIL:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+
+        default: {
+            return state
+        }
+
+
+    }
+}
