1947ce91d822bcc9f1f348f627b9aebaf00c124b
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-08 19:25:41 +0530

    add online status of clients state

--- client/src/component/Inbox/inboxReducer.js
+++ client/src/component/Inbox/inboxReducer.js
@@ -1,10 +1,12 @@
-import { FETCH_ALL_CLIENTS_DETAILS, FETCH_ALL_CLIENTS_LAST_MESSAGE, FETCH_ALL_CLIENTS_LIST, UPDATE_ALL_CLIENTS_LIST, UPDATE_CLIENT_DETAILS, UPDATE_CLIENT_LAST_MESSAGE, FETCH_ALL_CHATS_WITH_CLIENT, UPDATE_ALL_CHATS_WITH_CLIENT } from "./inboxConstant";
+import { FETCH_ALL_CLIENTS_DETAILS, FETCH_ALL_CLIENTS_LAST_MESSAGE, FETCH_ALL_CLIENTS_LIST, UPDATE_ALL_CLIENTS_LIST, UPDATE_CLIENT_DETAILS, UPDATE_CLIENT_LAST_MESSAGE, FETCH_ALL_CHATS_WITH_CLIENT, UPDATE_ALL_CHATS_WITH_CLIENT, FETCH_ONLINE_STATUS_OF_CLIENTS, UPDATE_ONLINE_STATUS_OF_CLIENTS } from "./inboxConstant";
 
 export const INBOX_DETAILS_INITIAL_STATE = {
     listOfAllClients: null,
     allClientsDetails: null,
     allClientUserLastMessage: null,
     inboxMessages: null,
+    onlineStatusOfClients: null,
+    inboxMessagesLoading: false,
 }
 
 export const inboxReducer = (state, action) => {
@@ -48,15 +50,30 @@ export const inboxReducer = (state, action) => {
             // console.log(action.payload);
             return {
                 ...state,
+                inboxMessagesLoading: true,
                 inboxMessages: action.payload,
             }
         }
         case UPDATE_ALL_CHATS_WITH_CLIENT: {
             return {
                 ...state,
+                inboxMessagesLoading: true,
                 inboxMessages: action.payload,
             }
         }
+        case FETCH_ONLINE_STATUS_OF_CLIENTS: {
+            return{
+                ...state,
+                onlineStatusOfClients: action.payload,
+            }
+        }
+        case UPDATE_ONLINE_STATUS_OF_CLIENTS:{
+            return{
+                ...state,
+                onlineStatusOfClients: action.payload,
+            }
+        }
+        
         default:
             return state;
     }
