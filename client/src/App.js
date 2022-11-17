b5e76d8864b08582a616ee0fdc0f709e0b24e37e
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-17 19:17:53 +0530

    notFoundPage & socketCtx & windowCtx create

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, createContext, useContext, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./app.css"
@@ -20,14 +20,24 @@ import { Test } from './component/Test/Test';
 import { CurrentlySelectedClientChat } from './component/CurrentlySelectedClientChat/CurrentlySelectedClientChat';
 import { loadUser } from './actions/userAction';
 import store from './store';
+import { NotFoundPage } from './component/NotFoundPage/NotFoundPage';
+import { CreateGig } from './component/CreateGig/CreateGig';
 
+import { SocketContext } from './context/socket/socket';
+import { socket } from './context/socket/socket';
+
+export const windowContext = createContext();
+
+// export const socket = io.connect("http://localhost:4000");
 
 const App = () => {
 	let { id } = useParams();
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
-	const { user, isAuthenticated } = useSelector(state => state.loggedUser);
+	const { user, isAuthenticated } = useSelector(state => state.user);
 
+	const [windowWidth, setWindowWidth] = useState(0);
+	const [windowHeight, setWindowHeight] = useState(0);
 
 	const dimBackground = useSelector(state => state.dimBackground);
 	let height = document.documentElement.offsetHeight;
@@ -35,39 +45,65 @@ const App = () => {
 	const pageheight = window.innerHeight;
 	height = Math.max(pageheight, height);
 
-	const { loading, error, gigs, gigsCount } = useSelector(state => state.gigs);
 
-	console.log(isAuthenticated);
-
-	useEffect(()=> {
+	useEffect(() => {
 		// i have put .then to resolve the promise and lead us to the page where we come from this is working now
-		store.dispatch(loadUser()).then(()=> {
+		store.dispatch(loadUser()).then(() => {
 			// console.log("app navigate is running");
 			navigate();
 		});
-	},[])
-	
+	}, [])
+
+
+
+
+
+	let resizeWindow = () => {
+		setWindowWidth(window.innerWidth);
+		setWindowHeight(window.innerHeight);
+	};
+
+	useEffect(() => {
+		// console.log(windowWidth);
+		resizeWindow();
+		window.addEventListener("resize", resizeWindow);
+		return () => window.removeEventListener("resize", resizeWindow);
+	}, [resizeWindow]);
+
+
+
+
+
+
+
 	return (
-		<Fragment>
-			<Header></Header>
-			<ToastContainer />
-			<Routes>
-				<Route exact path='/get/client/seller/chat:id' element={<CurrentlySelectedClientChat />}></Route>
-				<Route exact path='/get/all/messages/for/current/user' element={<Inbox />}></Route>
-				<Route exact path='/' element={<Home />} />
-				<Route exact path='/login' element={<Login />} />
-				{/* {
+
+		<>
+			<windowContext.Provider value={{ windowWidth, windowHeight }}>
+				<SocketContext.Provider value={socket}>
+					<Header></Header>
+					<ToastContainer />
+					<Routes>
+						<Route exact path='/get/client/seller/chat:id' element={<CurrentlySelectedClientChat />}></Route>
+						<Route exact path='/get/all/messages/for/current/user' element={<Inbox />}></Route>
+						<Route exact path='/' element={<Home />} />
+						<Route exact path='/login' element={<Login />} />
+						{/* {
 					gigs && gigs.map(gig => (
 						<Sidebar gig={gig} key={gig._id} />
 					))
 				} */}
-				{/* <div style={{ height: height - (width > 600 ? 81 : 143) }} className={'search-bar-dim-background ' + (dimBackground ? "visible" : null)}></div> */}
+						{/* <div style={{ height: height - (width > 600 ? 81 : 143) }} className={'search-bar-dim-background ' + (dimBackground ? "visible" : null)}></div> */}
 
-				<Route exact path='/user/:id' element={<UserDetail />} />
-				<Route exact path='/gig/details/:id' element={<GigDetail />} />
-			</Routes>
-			{/* <Footer /> */}
-		</Fragment>
+						<Route exact path='/user/:id' element={<UserDetail />} />
+						<Route exact path='/gig/details/:id' element={<GigDetail />} />
+						<Route path='*' element={<NotFoundPage />} />
+						<Route exact path='/gig/create/new/gig' element={<CreateGig />}></Route>
+					</Routes>
+					{/* <Footer /> */}
+				</SocketContext.Provider>
+			</windowContext.Provider>
+		</>
 	);
 }
 
