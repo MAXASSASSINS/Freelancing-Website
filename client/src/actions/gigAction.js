acbe3efd5dcc421be894521939f0c6e82a63967b
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-11-17 19:26:19 +0530

    update all gigs add

--- client/src/actions/gigAction.js
+++ client/src/actions/gigAction.js
@@ -2,6 +2,7 @@ import {
     ALL_GIG_FAIL, ALL_GIG_REQUEST, ALL_GIG_SUCCESS,
     USER_GIGS_REQUEST, USER_GIGS_SUCCESS, USER_GIGS_FAIL,
     GIG_DETAIL_FAIL, GIG_DETAIL_REQUEST, GIG_DETAIL_SUCCESS,
+    UPDATE_ALL_GIGS_REQUEST, UPDATE_ALL_GIGS_SUCCESS, UPDATE_ALL_GIGS_FAIL,
     CLEAR_ERRORS
 } from "../constants/gigConstants.js";
 import axios from 'axios'
@@ -10,7 +11,7 @@ import axios from 'axios'
 export const getAllGig = () => async (dispatch) => {
     try {
         dispatch({ type: ALL_GIG_REQUEST });
-        const {data} = await axios.get('/gig/gigs');
+        const { data } = await axios.get('/gig/gigs');
         // const data = await fetchedData.json();
 
         dispatch({
@@ -29,7 +30,7 @@ export const getUserGigs = (id) => async (dispatch) => {
     try {
         dispatch({ type: USER_GIGS_REQUEST });
 
-        const {data} = await axios.get(`/gig/gigs/user/${id}`);
+        const { data } = await axios.get(`/gig/gigs/user/${id}`);
         // const data = await fetchedData.json();
 
         dispatch({
@@ -48,9 +49,9 @@ export const getGigDetail = (id) => async (dispatch) => {
     try {
         dispatch({ type: GIG_DETAIL_REQUEST });
 
-        const {data} = await axios.get(`/gig/details/${id}`);
+        const { data } = await axios.get(`/gig/details/${id}`);
         // const data = await fetchedData.json();
-        
+
         dispatch({
             type: GIG_DETAIL_SUCCESS,
             payload: data,
@@ -61,6 +62,13 @@ export const getGigDetail = (id) => async (dispatch) => {
             payload: error.response.data.message,
         })
     }
+}
+
+export const updateAllGigs = (data) => async dispatch => {
+    dispatch({
+        type: UPDATE_ALL_GIGS_SUCCESS,
+        payload: data
+    })
 }
 
 export const clearErrors = () => async (dispatch) => {
