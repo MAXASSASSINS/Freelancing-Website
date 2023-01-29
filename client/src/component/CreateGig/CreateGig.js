1e4c54d7a888795063473e8e779023bce79394fe
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-01-29 23:43:08 +0530

    clean up of select input component

--- client/src/component/CreateGig/CreateGig.js
+++ client/src/component/CreateGig/CreateGig.js
@@ -17,7 +17,6 @@ import { FileDropIcon } from '../FileDropIcon/FileDropIcon'
 import { RoundNumberIcon } from '../RoundNumberIcon/RoundNumberIcon'
 import { fontWeight } from '@mui/system'
 import { TextArea } from '../TextArea/TextArea'
-import { SelectInput } from '../SelectInput/SelectInput'
 import { Test } from '../Test/Test'
 import { BsDash } from 'react-icons/bs'
 import { questionTypeData } from './createGigData'
@@ -27,7 +26,6 @@ import { AiOutlineEllipsis } from 'react-icons/ai'
 import SelectInput2 from '../SelectInput/SelectInput2'
 import { IoClose } from 'react-icons/io5'
 import { AddorUpdateQuestion } from './AddorUpdateQuestion'
-import { convertFromRaw, convertToRaw } from 'draft-js'
 
 export const CreateGig = () => {
   const navigate = useNavigate();
@@ -420,33 +418,12 @@ export const CreateGig = () => {
             {/* <div>15 characters minimum</div> */}
           </div>
           <div className='category-section'>
-            {/* <SelectInput
-              option={'select a category'}
-              defaultOption={'Select a category'}
-              data={categoriesData}
-              setOption={setSelectedCategory}
-              isSubOption={false}
-              value={selectedCategory}
-              style={{ textTransform: 'uppercase' }}
-            /> */}
             <SelectInput2
               defaultOption={'Select a category'}
               data={categoriesData}
               style={{ textTransform: 'uppercase' }}
               getChoosenOption={getSelectedCategory}
             />
-
-            {/* <SelectInput
-              value={selectedSubCategory}
-              defaultOption={'Select a sub-category'}
-              data={subCategoriesData}
-              setOption={setSelectedSubCategory}
-              isSubOption={true}
-              mainOption={selectedCategory ? selectedCategory : null}
-              defaultMainOption={'Select a category'}
-              style={{ textTransform: 'uppercase' }}
-            /> */}
-
             <SelectInput2
               defaultOption={'Select a sub-category'}
               data={selectedCategory && getSubCategoryList(selectedCategory)}
@@ -567,95 +544,6 @@ export const CreateGig = () => {
             />
             :
             !showQuestions ?
-              /* <div className='add-question-wrapper'>
-                <header>
-                  <h6>Add a question</h6>
-                  <CheckInput
-                    label='Required'
-                    reference={questionRequiredInputRef}
-                  />
-                </header>
-  
-                <div className='question-box'>
-                  <TextArea
-                    maxLength={400}
-                    minLength={2}
-                    placeholder={'Request necessary details such as dimensions, brand guidelines, and more'}
-                    style={{ height: "8rem" }}
-                    getText={getQuestionTitle}
-                  />
-                  <div
-                    className='question-description-warning'
-                    style={{ display: (questionTitle.length < 2 && warningEnabled) ? "block" : "none" }}
-                  >
-                    Description should be at least 2 characters long
-                  </div>
-                </div>
-  
-                <div className='question-form-and-options'>
-                  <h6>Get it in a form of:</h6>
-                  <div className='question-type-wrapper'>
-                    <div>
-                      <SelectInput2
-                        data={questionTypeData}
-                        defaultOption={'Free Text'}
-                        getChoosenOption={getQuestionType}
-                        style={{ width: "15rem" }}
-                      />
-                    </div>
-  
-                    {
-                      questionType === 'Multiple Choice' &&
-                      <div className=''>
-                        <CheckInput
-                          label='Enable to choose more than 1 option'
-                          reference={enableMultipleOptionsInputRef}
-                        />
-                      </div>
-                    }
-                  </div>
-                </div>
-  
-                {
-                  questionType === 'Multiple Choice' &&
-                  <div className='add-options-wrapper'>
-                    {
-                      options.map((option, index) => (
-                        <div className='option' key={'option' + index}>
-                          <div className='option-wrapper'>
-                            <input
-                              placeholder='Add Option'
-                              onChange={(e) => handleOptionChange(e, index)}
-                              value={option}
-                            />
-                            <span
-                              style={{ display: options.length <= 2 ? "none" : "block" }}
-                              className='close-icon'
-                              onClick={() => handleRemoveOption(index)}
-                            >
-                              <IoClose />
-                            </span>
-                          </div>
-                          <div
-                            className='option-warning'
-                            style={{ display: (options[index].length == 0 && warningEnabled) ? "block" : "none" }}
-                          >
-                            Description should be at least 1 character long
-                          </div>
-                        </div>
-                      ))
-                    }
-                    <button onClick={handleAddNewOption}>+ Add New Option</button>
-                  </div>
-                }
-  
-                <div className='add-cancel-buttons-wrapper'>
-                  <button className='cancel-add' onClick={handleCancelAdd}>Cancel</button>
-                  <button className='add-question' onClick={handleAddQuestion}>Add</button>
-                </div>
-  
-              </div> */
-
               <AddorUpdateQuestion
                 getRequiredStatusOfQuestion={getRequiredStatusOfQuestion}
                 getMultipleOptionSelectionStatus={getMultipleOptionSelectionStatus}
