0697e570a728cbbad938d379f8613aaaca13ad24
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-01-29 23:28:43 +0530

    provides ref and lifting state up functionality

--- client/src/component/TextArea/TextArea.js
+++ client/src/component/TextArea/TextArea.js
@@ -1,10 +1,20 @@
 import { fontSize } from '@mui/system';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react'
 import './textArea.css'
 
-export const TextArea = ({ maxLength, minLength, style, placeholder, defaultText, warning }) => {
+export const TextArea = forwardRef(({ maxLength, minLength, style, placeholder, 
+    defaultText, warning, reference, getText } , ref) => {
 
-    const [currentTextLength, setCurrentTextLength] = useState(minLength);
+    const [currentTextLength, setCurrentTextLength] = useState(defaultText ? defaultText.length : 0);
+    const [text, setText] = useState(defaultText ? defaultText : "");
+
+    const handleChange = (e) => {
+        setCurrentTextLength(e.target.value.length);
+        setText(e.target.value);
+        // console.log(getText);
+        getText(e.target.value);
+    }
+
 
     return (
         <div className='textarea-main'  >
@@ -16,8 +26,9 @@ export const TextArea = ({ maxLength, minLength, style, placeholder, defaultText
                 autoComplete='off'
                 autoCapitalize='off'
                 defaultValue={defaultText}
-                onChange={(e) => setCurrentTextLength(e.target.value.length)}
+                onChange={handleChange}
                 style={style}
+                ref={reference}
             >
             </textarea>
             <div className='textarea-footer'>
@@ -31,4 +42,4 @@ export const TextArea = ({ maxLength, minLength, style, placeholder, defaultText
             </div>
         </div>
     )
-}
+})
