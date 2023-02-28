02e653d4caa86e923e3594f21444e3dc85112e07
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-01 00:45:43 +0530

    implement sending sms using twilio

---
+++ server/utils/twilio.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import twilio from 'twilio';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const twilioClient = twilio(accountSid, authToken);
+
+export const sendSMS = (req, res) => {
+  console.log("hit");
+  res.header('Content-Type', 'application/json');
+  twilioClient.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+}
