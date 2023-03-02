452fe300a5014b27133b07b968dfce9fc48a6278
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-02 22:35:07 +0530

    added verify number and verify code funtions

--- server/utils/twilio.js
+++ server/utils/twilio.js
@@ -1,4 +1,3 @@
-import express from 'express';
 import twilio from 'twilio';
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -7,7 +6,6 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = twilio(accountSid, authToken);
 
 export const sendSMS = (req, res) => {
-  console.log("hit");
   res.header('Content-Type', 'application/json');
   twilioClient.messages
     .create({
@@ -22,4 +20,19 @@ export const sendSMS = (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+}
+
+export const verifyNumber = (req, res) => {
+  twilioClient.verify.v2.services('VA571fee7d19f3e28ef9a285e861a341b1')
+    .verifications
+    .create({ to: req.body.to, channel: 'sms' })
+    .then(verification => console.log(verification.status));
+}
+
+export const verifyCode = (req, res) => {
+  twilioClient.verify.v2.services('VA571fee7d19f3e28ef9a285e861a341b1')
+    .verificationChecks
+    .create({ to: req.body.to, code: req.body.code })
+    .then(verification_check => console.log(verification_check.status))
+    .catch(err => console.log(err));
 }
