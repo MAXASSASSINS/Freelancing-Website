fc3d3eaf94495a265801de7caff9271ae83afe8a
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-30 16:59:01 +0530

    custom ref & editor state changes

--- client/src/component/TextEditor/TextEditor.js
+++ client/src/component/TextEditor/TextEditor.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useState, forwardRef, useImperativeHandle } from 'react'
 import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 import '../../../node_modules/draft-js/dist/Draft.css';
 import './textEditor.css';
-
+import { convertFromHTML, convertToHTML } from 'draft-convert'
 import { inlineStyleButtons, blockTypeButtons, getIcon, InlineStyleButton, BlockStyleButton } from './textEditorUtil';
 
-export const TextEditor = ({ maxLength, hideToolbar, hideFooter }) => {
+export const TextEditor = forwardRef(({ maxLength, hideToolbar, hideFooter, readOnly }, ref) => {
     const [length, setLength] = useState(0);
-    const [editorState, setEditorState] = React.useState(
+    const [editorState, setEditorState] = useState(
         () => EditorState.createEmpty(),
     );
 
     const handleChange = (editorState) => {
+        console.log(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
         setEditorState(editorState);
         setLength(editorState.getCurrentContent().getPlainText().length);
     };
@@ -36,10 +37,27 @@ export const TextEditor = ({ maxLength, hideToolbar, hideFooter }) => {
         }
     }
 
+    useImperativeHandle(ref, () => ({
+        getDescriptionLength() {
+            return length;
+        },
+        getDescription() {
+            const contentState = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
+            const desc = editorState.getCurrentContent().getPlainText();
+            return { contentState, desc };
+        },
+        setDescriptionComingFromParent(description) {
+            let contentState = convertFromRaw(JSON.parse(description));
+            let editorState = EditorState.createWithContent(contentState);
+            setLength(editorState.getCurrentContent().getPlainText().length);
+            setEditorState(editorState);
+        }
+    }), [editorState,length]);
+
 
 
     return (
-        <div className='textEditor-main'>
+        <div className='textEditor-main' ref={ref}>
             <div className='textEditor-toolbar' style={{ display: hideToolbar ? "none" : null }}>
                 {
                     inlineStyleButtons.map((button, index) => {
@@ -76,7 +94,9 @@ export const TextEditor = ({ maxLength, hideToolbar, hideFooter }) => {
                 onChange={handleChange}
                 customStyleMap={styleMap}
                 customStyleFn={() => {
-                    return { lineHeight: 1.5 }
+                    return { 
+                        lineHeight: 1.5,
+                    }
                 }}
             />
 
@@ -85,7 +105,7 @@ export const TextEditor = ({ maxLength, hideToolbar, hideFooter }) => {
             </div>
         </div>
     );
-}
+});
 
 
 
