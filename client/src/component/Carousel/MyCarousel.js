f0c1a34edae52dac115c73e46e60f340e459bffd
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-30 16:47:27 +0530

    add default image and some cleanup

--- client/src/component/Carousel/MyCarousel.js
+++ client/src/component/Carousel/MyCarousel.js
@@ -1,12 +1,12 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line
 import Carousel from 'react-bootstrap/Carousel';
 import './myCarousel.css'
-
+import { IoMdImages } from 'react-icons/io';
 
 export const MyCarousel = ({ gig }) => {
     const [arrows, setArrows] = useState(false);
-
+    
     const showArrows = () => {
         setArrows(true);
     }
@@ -19,26 +19,21 @@ export const MyCarousel = ({ gig }) => {
     return (
         <div className='slides-preview' onMouseEnter={showArrows} onMouseLeave={hideArrows}>
             {
-                <Carousel interval={null} touch={true} indicators={false} controls={arrows}>
-                    {
-                        gig && gig.images.map(image => (
-                            <Carousel.Item key={image._id}>
-                                <img src={image.imgUrl} alt={gig.title} ></img>
-                            </Carousel.Item>
-                        ))
-                    }
-
-                    
-                    {/* <Carousel.Item>
-                        <img src="../images/img1.jpeg" alt="gig image"></img>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="../images/img2.webp" alt="gig image"></img>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="../images/img3.jpeg" alt="gig image"></img>
-                    </Carousel.Item> */}
-                </Carousel>
+                gig && gig.images.length === 0 ?
+                    <div className='no-images-container'>
+                        <IoMdImages className='no-images-icon'></IoMdImages>
+                    </div>
+                    :
+                    <Carousel interval={null} touch={true} indicators={false} controls={arrows}>
+                        {
+                            gig.images.map(image => (
+                                image && 
+                                <Carousel.Item key={image._id}>
+                                    <img className='carousel-img' src={image.imgUrl} alt={gig.title} ></img>
+                                </Carousel.Item>
+                            ))
+                        }
+                    </Carousel>
             }
         </div>
     );
