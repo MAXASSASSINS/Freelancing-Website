0661e6bc3cc013f47e3391ac733bdb94b54e076a
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-30 17:06:12 +0530

    add twilio config file

--- server/utils/twilio.js
+++ server/utils/twilio.js
@@ -26,13 +26,43 @@ export const verifyNumber = (req, res) => {
   twilioClient.verify.v2.services('VA571fee7d19f3e28ef9a285e861a341b1')
     .verifications
     .create({ to: req.body.to, channel: 'sms' })
-    .then(verification => console.log(verification.status));
+    .then(verification => {
+      console.log(verification.status);
+      res.status(200).json({ success: true })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ success: false, error: err })
+    });
 }
 
 export const verifyCode = (req, res) => {
+  console.log(req.body);
+  try {
+    twilioClient.verify.v2.services('VA571fee7d19f3e28ef9a285e861a341b1')
+      .verificationChecks
+      .create({ to: req.body.to, code: req.body.code })
+      .then(verification_check => {
+        console.log(verification_check.status);
+        if(verification_check.status === 'approved')
+          res.status(200).json({ success:  true})
+        else
+          res.status(202).json({ success: false, error: 'Invalid code' })
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json({ success: false, error: err })
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false, error: err })
+  }
+}
+
+export const verifyByCall = (req, res) => {
   twilioClient.verify.v2.services('VA571fee7d19f3e28ef9a285e861a341b1')
-    .verificationChecks
-    .create({ to: req.body.to, code: req.body.code })
-    .then(verification_check => console.log(verification_check.status))
+    .verifications
+    .create({ to: req.body.to, channel: 'voice' })
+    .then(verification => console.log(verification.status))
     .catch(err => console.log(err));
 }
