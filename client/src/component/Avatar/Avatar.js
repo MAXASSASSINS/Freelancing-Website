42a58e97283945333caabed9f81d2947ec90290d
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:31:44 +0530

    avatar component created

---
+++ client/src/component/Avatar/Avatar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Avatar = ({ avatarUrl, userName, onlineStatus, width, alt }) => {
+
+  return (
+    <div style={{ width: width, height: width }}>
+      {onlineStatus !== undefined && (
+        <div
+          className="absolute rounded-full border-2 border-white w-4 h-4 bottom-0 -right-0.5"
+          style={{
+            backgroundColor: onlineStatus ? "#1dbf73" : "#a6a5a5",
+          }}
+        />
+      )}
+      {avatarUrl ? (
+        <img src={avatarUrl} alt={alt} className="w-full h-full rounded-full" />
+      ) : (
+        <div className="w-full bg-no_focus h-full rounded-full flex items-center justify-center">
+          <p className='text-off_white font-medium text-lg'>
+            {userName[0].toUpperCase()}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
