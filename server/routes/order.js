144556422b4f1f90998915e6e8c920d625d90b16
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:52:01 +0530

    payment add

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,15 +1,20 @@
 import express from "express"
-import { getAllOrders, getOrderDetails, myOrders, newOrder, updateOrderStatus } from "../controllers/orderContoller.js";
+import { getAllOrders, getOrderDetails, getStripePublishableKey, myOrders, newOrder, packagePayment, updateOrderStatus, updateOrder } from "../controllers/orderContoller.js";
 import { authorisedRoles, isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router();
 
 
 router.post("/order/new", isAuthenticated, newOrder);
-router.get("/order/:id", isAuthenticated, authorisedRoles("admin"), getOrderDetails);
+router.get("/order/details/:id", isAuthenticated, getOrderDetails);
 router.get("/orders/me", isAuthenticated, myOrders);
 
 router.get('/orders/getAll',isAuthenticated, authorisedRoles("admin"), getAllOrders);
 router.put('/orders/updateOrderStatus/:id', isAuthenticated, authorisedRoles("admin"), updateOrderStatus);
 
+router.put('/order/update/:id', isAuthenticated, updateOrder);
+
+router.post('/package/payment',isAuthenticated, packagePayment);
+
+router.get('/get/stripe/publishable/key', isAuthenticated, getStripePublishableKey);
 export default router;
