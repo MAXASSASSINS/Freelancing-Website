4e4c29a249fe3602e5f2dd597c955d3ac9099a92
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:32:36 +0530

    issue fixed

--- client/src/component/CheckInput/CheckInput.js
+++ client/src/component/CheckInput/CheckInput.js
@@ -1,48 +1,55 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react'
-import './checkInput.css'
-import {GoCheck} from 'react-icons/go'
-import '../../utility/color'
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import "./checkInput.css";
+import { GoCheck } from "react-icons/go";
+import "../../utility/color";
 
-export const CheckInput = forwardRef(({ label, labelColor, reference, defaultValue = false, getInputCheckedVal }, ref) => {
-
+export const CheckInput = forwardRef(
+  (
+    { label, labelColor, reference, defaultValue = false, getInputCheckedVal },
+    ref
+  ) => {
     const [isChecked, setIsChecked] = useState(defaultValue);
 
     const handleChange = (e) => {
-        // console.log(getInputCheckedVal);
-        // console.log(e.target.checked);
-        setIsChecked(e.target.checked);
-        getInputCheckedVal && getInputCheckedVal(e.target.checked);
-    }
+      // console.log(getInputCheckedVal);
+      // console.log(e.target.checked);
+      setIsChecked(e.target.checked);
+      getInputCheckedVal && getInputCheckedVal(e.target.checked);
+    };
 
-    useImperativeHandle(ref, () => ({
+    useImperativeHandle(
+      ref,
+      () => ({
         currValue: isChecked,
         setIsCheckedComingFromParent: (val) => {
-            setIsChecked(val);
-        }
-    }), [isChecked]);
+          setIsChecked(val);
+        },
+      }),
+      [isChecked]
+    );
 
     // console.log(isChecked);
 
     return (
-        <label className='check-input-main noSelect'>
-            <input
-                type="checkbox"
-                onChange={handleChange}
-                value={isChecked}
-                defaultChecked={defaultValue}
-            />
-            <span
-                className={`checkbox ${isChecked ? "checkbox--active" : ""}`}
-                aria-hidden="true"
-            >
-                {
-                    isChecked &&
-                    <GoCheck style={{color: "#62646a", fontWeight: "700"}}/>
-                }
-            </span>
-            <div style={{color: labelColor ? labelColor : "#393e46"}}>
-                {label}
-            </div>
-        </label>
-    )
-})
+      <label className="check-input-main noSelect">
+        <input
+          type="checkbox"
+          onChange={handleChange}
+          value={isChecked}
+          defaultChecked={defaultValue}
+        />
+        <span
+          className={`checkbox ${isChecked ? "checkbox--active" : ""}`}
+          aria-hidden="true"
+        >
+          {isChecked && (
+            <GoCheck style={{ color: "#62646a", fontWeight: "700" }} />
+          )}
+        </span>
+        <div style={{ color: labelColor ? labelColor : "inherit" }}>
+          {label}
+        </div>
+      </label>
+    );
+  }
+);
