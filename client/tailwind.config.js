d3bb3d2a66fd5504fa1eead70b7f90361601bf5a
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:29:13 +0530

    tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark_grey': '#222831',
+        'light_grey': '#393e46',
+        'green': '#1dbf73',
+        'light_green': '#1dbf73',
+        'off_white': '#EEEEEE',
+        'no_focus': '#a6a5a5',
+        'icons': '#74767e',
+        'gold': '#ffbe5b',
+        'border': '#F1EEE9',
+        'separator': '#efeff0',
+        'light_heading': '#62646a',
+        'dark_separator': '#e4e5e7',
+        'light_background': '#d5d4d2',
+        'link': '#4a73e8',
+        'warning': '#f74040',
+        'error': '#f74040',
+        'green_hover': '#19a463',
+        'warning_background_color': '#ffeded',
+        'green_background_color': '#d5f6d7',
+      },
+      borderWidth:{
+        '1': '1px',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
