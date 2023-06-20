3ae23a8306e7288d2fb2fd91330233e8eb9ac896
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:44:13 +0530

    more colors add

--- client/src/utility/color.js
+++ client/src/utility/color.js
@@ -1,16 +1,39 @@
-export const dark_grey_color = '#222831';
-export const light_grey_color = '#393e46';
-export const green_color = '#1dbf73';
-export const light_green_color = '#1dbf73';
-export const off_white_color = '#EEEEEE';
-export const no_focus_color = '#a6a5a5';
-export const icons_color = '#74767e';
-export const gold_color = '#ffbe5b';
-export const border_color = '#F1EEE9';
-export const separator_color = '#efeff0';
-export const light_heading_color = '#62646a';
-export const dark_separator_color = '#e4e5e7';
-export const light_background_color = '#d5d4d2';
-export const link_color = '#4a73e8';
-export const warning_color = '#f74040';
-export const green_hover = '#19a463';
+export const dark_grey_color = "#222831";
+export const light_grey_color = "#393e46";
+export const green_color = "#1dbf73";
+export const light_green_color = "#1dbf73";
+export const off_white_color = "#EEEEEE";
+export const no_focus_color = "#a6a5a5";
+export const icons_color = "#74767e";
+export const gold_color = "#ffbe5b";
+export const border_color = "#F1EEE9";
+export const separator_color = "#efeff0";
+export const light_heading_color = "#62646a";
+export const dark_separator_color = "#e4e5e7";
+export const light_background_color = "#d5d4d2";
+export const link_color = "#4a73e8";
+export const warning_color = "#f74040";
+export const green_hover = "#19a463";
+export const warning_background_color = "#ffeded";
+export const green_background_color = "#D5F6D7";
+
+export const colors = new Map([
+  ["dark_grey_color", dark_grey_color],
+  ["light_grey_color", light_grey_color],
+  ["green_color", green_color],
+  ["light_green_color", light_green_color],
+  ["off_white_color", off_white_color],
+  ["no_focus_color", no_focus_color],
+  ["icons_color", icons_color],
+  ["gold_color", gold_color],
+  ["border_color", border_color],
+  ["separator_color", separator_color],
+  ["light_heading_color", light_heading_color],
+  ["dark_separator_color", dark_separator_color],
+  ["light_background_color", light_background_color],
+  ["link_color", link_color],
+  ["warning_color", warning_color],
+  ["green_hover", green_hover],
+  ["warning_background_color", warning_background_color],
+  ["green_background_color", green_background_color],
+]);
