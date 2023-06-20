c606cc6a2baa76f2a70825a39ebd0683cc490d2c
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:50:51 +0530

    linting fix

--- server/routes/gig.js
+++ server/routes/gig.js
@@ -1,21 +1,28 @@
-import express from 'express'
-import { createGig, createGigReview, deleteGig, getAllGigs, getAllReviews, getGig, getUserGigs, updateGig } from '../controllers/gigController.js';
-import { authorisedRoles, isAuthenticated } from '../middleware/auth.js';
-
+import express from "express";
+import {
+  createGig,
+  createGigReview,
+  deleteGig,
+  getAllGigs,
+  getAllReviews,
+  getGig,
+  getUserGigs,
+  updateGig,
+} from "../controllers/gigController.js";
+import { authorisedRoles, isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/gig/create', isAuthenticated, createGig);
+router.post("/gig/create", isAuthenticated, createGig);
 
-router.get('/gig/gigs', getAllGigs);
-router.get('/gig/details/:id', getGig);
-router.get('/gig/gigs/user/:id', getUserGigs);
+router.get("/gig/gigs", getAllGigs);
+router.get("/gig/details/:id", getGig);
+router.get("/gig/gigs/user/:id", getUserGigs);
 
-router.put('/gig/update/:id', isAuthenticated, updateGig);
-router.delete('/gig/delete/:id', isAuthenticated, deleteGig);
+router.put("/gig/update/:id", isAuthenticated, updateGig);
+router.delete("/gig/delete/:id", isAuthenticated, deleteGig);
 
-router.put('/review', isAuthenticated, createGigReview);
-router.get('/getAllReviews', getAllReviews);
+router.put("/review", isAuthenticated, createGigReview);
+router.get("/getAllReviews", getAllReviews);
 
 export default router;
-
