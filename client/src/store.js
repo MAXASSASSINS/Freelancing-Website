43f00f536c14a9bc26a2b9cf84048064d518c9c1
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-06-21 00:30:17 +0530

    order reducer add

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,8 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { userGigsDetailsReducer, gigReducer, gigDetailReducer } from './reducers/gigReducer.js';
 import { dimBackgroundReducer } from './reducers/dimBackgroundReducer.js';
 import { gigUserReducer, loginUserReducer, userReducer } from './reducers/userReducers.js';
+import { orderDetailReducer, orderReducer, userOrdersDetailsReducer } from './reducers/orderReducer.js';
+
 
 const reducer = combineReducers({
     gigs: gigReducer,
@@ -13,6 +15,11 @@ const reducer = combineReducers({
     gigUser: gigUserReducer,
     gigDetail: gigDetailReducer,
     loggedUser: loginUserReducer,
+    orderDetail: orderDetailReducer,
+    orders: orderReducer,
+    userOrders: userOrdersDetailsReducer,
+    
+
 })
 
 let initialsState = {};
