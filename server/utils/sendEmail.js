27281e1e70e1e2117a86272f721e1b4e98301035
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-01 01:05:30 +0530

    send email is now working

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,18 +2,19 @@ import nodeMailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
-    host: "smpt.gmail.com",
-    port: 465,
-    service: process.env.SMPT_SERVICE,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    // service: process.env.SMTP_SERVICE,
     auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
     },
   });
 
   const mailOptions = {
     from: process.env.SMPT_MAIL,
-    to: options.email,
+    to: options.to,
     subject: options.subject,
     text: options.message,
   };
