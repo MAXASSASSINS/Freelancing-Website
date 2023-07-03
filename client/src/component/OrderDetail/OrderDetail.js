76e05d3fe6854440bcf10476c50b0d26a33aef33
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-03 21:42:44 +0530

    fixing image height

---
+++ client/src/component/OrderDetail/OrderDetail.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const OrderDetail = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>Activity</li>
+            <li>Details</li>
+            <li>Requirements</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
