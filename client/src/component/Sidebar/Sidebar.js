73e248d94d4cbc3c0e0f516797a6bbf5cf403948
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-04 19:58:34 +0530

    adding error & navigate only when no error

--- client/src/component/Sidebar/Sidebar.js
+++ client/src/component/Sidebar/Sidebar.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import "./sidebar.css";
 import { Avatar } from "../Avatar/Avatar";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../actions/userAction";
 
 export const Sidebar = () => {
-  const { user, loading, isAuthenticated } = useSelector((state) => state.user);
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, loading, isAuthenticated, error } = useSelector(
+    (state) => state.user
+  );
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => {
@@ -15,6 +22,11 @@ export const Sidebar = () => {
 
   const hideSidebar = () => {
     setSidebar(false);
+  };
+
+  const handleLogOut = async () => {
+    dispatch(logoutUser());
+    if (!error) navigate("/");
   };
 
   return (
@@ -46,22 +58,31 @@ export const Sidebar = () => {
 
             <div className="user-name">{user?.name || "Profile"}</div>
           </section>
-          <section className="sidebar-menu">
-            <a href="/" className="sidebar-menu-item">
+          <section onClick={hideSidebar} className="sidebar-menu">
+            <Link to="/" className="sidebar-menu-item">
               Home
-            </a>
-            <a href="/get/all/messages/for/current/user" className="sidebar-menu-item">
+            </Link>
+            <Link
+              to="/get/all/messages/for/current/user"
+              className="sidebar-menu-item"
+            >
               Inbox
-            </a>
-            <a href="/orders" className="sidebar-menu-item">
+            </Link>
+            <Link to="/orders" className="sidebar-menu-item">
               Manage Orders
-            </a>
-            <a href="/" className="sidebar-menu-item">
+            </Link>
+            <Link to="/" className="sidebar-menu-item">
               Lists
-            </a>
-            <a href="/" className="sidebar-menu-item">
-              Logout
-            </a>
+            </Link>
+            {isAuthenticated ? (
+              <div onClick={handleLogOut} className="sidebar-menu-item">
+                Logout
+              </div>
+            ) : (
+              <Link to="/login" className="sidebar-menu-item">
+                Login
+              </Link>
+            )}
           </section>
         </div>
         <div
