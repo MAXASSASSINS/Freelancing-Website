2b6bdd80a477434cc2816c06894148aba86e3505
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-05 01:21:47 +0530

    adding requirement submission time

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -112,6 +112,10 @@ const orderSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  requirementsSubmittedAt: {
+    type: Date,
+    default: Date.now,
+  },
   packageDetails: {
     packageTitle: {
       type: String,
