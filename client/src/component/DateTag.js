1043ee92016c8c88d16c8e61a4023ac92e3f0400
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-06 02:03:43 +0530

    add parse for non standard dates

--- client/src/component/DateTag.js
+++ client/src/component/DateTag.js
@@ -4,7 +4,7 @@ import Moment from "react-moment";
 export const DateTag = ({ date }) => {
   return (
     <div className="capitalize absolute left-0 text-icons font-semibold bg-separator w-24 pl-7 pr-4 p-0.5 text-xs rounded-tr-full rounded-br-full sm:text-sm sm:pl-10 sm:pr-4 sm:w-32">
-      <Moment format="MMM D">{date}</Moment>
+      <Moment parse={'DD-MM-YYYY'} format="MMM D">{date}</Moment>
     </div>
   );
 };
