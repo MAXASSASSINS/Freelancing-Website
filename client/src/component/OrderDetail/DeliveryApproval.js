98890890c3b4c36610d86cdf74cf5ac5b5034a75
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-07-20 00:59:41 +0530

    add error for maximum revision exceed

--- client/src/component/OrderDetail/DeliveryApproval.js
+++ client/src/component/OrderDetail/DeliveryApproval.js
@@ -8,12 +8,12 @@ import { IoClose } from "react-icons/io5";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { updateOrderDetail } from "../../actions/orderAction";
+import { ToastContainer, toast } from "react-toastify";
 
 const DeliveryApproval = ({ setFileLoading }) => {
   const params = useParams();
   const dispatch = useDispatch();
   const { orderDetail } = useSelector((state) => state.orderDetail);
-
 
   const [showRevision, setShowRevision] = useState(false);
   const [showFinalDeliveryConfirmation, setShowFinalDeliveryConfirmation] =
@@ -25,11 +25,19 @@ const DeliveryApproval = ({ setFileLoading }) => {
   };
 
   const handleOrderCompletion = async () => {
-    const {data} = await axios.post(`/order/completed/${params.id}`);
+    const { data } = await axios.post(`/order/completed/${params.id}`);
     console.log(data);
     dispatch(updateOrderDetail(data.order));
     setShowFinalDeliveryConfirmation(false);
-  }
+  };
+
+  const handleShowRevisionBox = () => {
+    if (orderDetail.packageDetails.revisions >= orderDetail.revisions.length) {
+      toast.error("You have exceeded you maximum revision requests");
+      return;
+    }
+    setShowRevision(true);
+  };
 
   return (
     <section className="flex ml-6  pb-4 mb-12 border-b gap-4 items-start">
@@ -48,11 +56,14 @@ const DeliveryApproval = ({ setFileLoading }) => {
             </p>
           </div>
           <div className="flex items-center gap-4 mt-6">
-            <button onClick={() => setShowFinalDeliveryConfirmation(true)} className="bg-primary text-white px-4 py-2 rounded-sm hover:bg-primary_hover hover:cursor-pointer">
+            <button
+              onClick={() => setShowFinalDeliveryConfirmation(true)}
+              className="bg-primary text-white px-4 py-2 rounded-sm hover:bg-primary_hover hover:cursor-pointer"
+            >
               Yes, I approve the delivery
             </button>
             <button
-              onClick={() => setShowRevision(true)}
+              onClick={handleShowRevisionBox}
               className="bg-primary text-white px-4 py-2 rounded-sm hover:bg-primary_hover hover:cursor-pointer"
             >
               I'm not ready yet
@@ -88,17 +99,26 @@ const DeliveryApproval = ({ setFileLoading }) => {
           <div className="bg-white w-5/6 sm:w-[60ch] rounded-sm">
             <div className="flex justify-between rounded-sm text-lg text-light_grey bg-dark_separator px-4 py-3">
               <h4>Approve Final Delivery</h4>
-              <IoClose onClick={() => setShowFinalDeliveryConfirmation(false)} className="hover:cursor-pointer" />
+              <IoClose
+                onClick={() => setShowFinalDeliveryConfirmation(false)}
+                className="hover:cursor-pointer"
+              />
             </div>
             <p className="p-4 leading-5 text-light_heading border-b border-b-dark_separator">
               Got everything you need? Great! Once you approve this delivery,
               your order will be marked as complete.
             </p>
             <div className="flex items-center justify-end p-4 gap-4 text-sm">
-              <button onClick={() => setShowFinalDeliveryConfirmation(false)} className="capitalize border border-light_heading text-light_heading font-semibold py-1.5 px-3 rounded hover:text-dark_grey hover:cursor-pointer">
+              <button
+                onClick={() => setShowFinalDeliveryConfirmation(false)}
+                className="capitalize border border-light_heading text-light_heading font-semibold py-1.5 px-3 rounded hover:text-dark_grey hover:cursor-pointer"
+              >
                 Cancel
               </button>
-              <button onClick={handleOrderCompletion} className="capitalize bg-primary py-1.5 px-3 hover:bg-primary_hover text-white rounded hover:cursor-pointer">
+              <button
+                onClick={handleOrderCompletion}
+                className="capitalize bg-primary py-1.5 px-3 hover:bg-primary_hover text-white rounded hover:cursor-pointer"
+              >
                 Approve Final Delivery
               </button>
             </div>
