c00090087761423559e2281367957826d605008f
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 20:27:43 +0530

    removing credentials

--- server/index.js
+++ server/index.js
@@ -28,8 +28,8 @@ app.use(cors(
     //     callback(new Error('Not allowed by CORS'))
     //   }
     // },
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: "http://localhost:3000",
+    // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
 ));
