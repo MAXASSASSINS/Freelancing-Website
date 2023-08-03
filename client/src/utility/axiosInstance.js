9caf7163cd718b3f9d42d81498fb7d703c60dcda
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 19:47:28 +0530

    fixing axios.instance

--- client/src/utility/axiosInstance.js
+++ client/src/utility/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // baseURL: "http://localhost:4000",
-    baseURL: "https://feelance-me-backend.vercel.app/",
+    baseURL: "https://feelance-me-backend.vercel.app",
     withCredentials: true,
 });
