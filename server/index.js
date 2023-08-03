ece067a225bd8349a9b163d2ccb8571c9d70b3d1
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 20:24:55 +0530

    changing origin

--- server/index.js
+++ server/index.js
@@ -28,9 +28,7 @@ app.use(cors(
     //     callback(new Error('Not allowed by CORS'))
     //   }
     // },
-    origin: [
-      'http://localhost:3000'
-    ],
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
