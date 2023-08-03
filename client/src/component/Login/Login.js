9fd2d450485a604c395a84753c3b8f3f21693fc7
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 15:33:59 +0530

    added error toast

--- client/src/component/Login/Login.js
+++ client/src/component/Login/Login.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loggedUser } from "../../actions/userAction";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 export const Login = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,10 @@ export const Login = () => {
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
+    if (!loginEmail || !loginPassword) {
+      toast.error("Please fill all the fields", {});
+      return;
+    }
     dispatch(loggedUser(loginEmail, loginPassword));
   };
 
@@ -27,25 +32,46 @@ export const Login = () => {
   }, [dispatch, isAuthenticated]);
 
   return (
-    <div className="mt-40 col-md-4 offset-md-4">
+    <div className="p-8 mt-20 md:mt-40 col-md-4 offset-md-4">
       <div className="text-center">
         <h2 className="text-5xl">Login</h2>
       </div>
       <form className="mt-8 flex flex-col gap-4" onSubmit={handleLoginSubmit}>
         <div className="form-group">
           <label className="mb-2" for="email">
-            Email-Id
+            Email
           </label>
-          <input onChange={(e) => setLoginEmail(e.target.value)} id="email" type="email" class="form-control" />
+          <input
+            onChange={(e) => setLoginEmail(e.target.value)}
+            id="email"
+            type="email"
+            class="form-control"
+          />
         </div>
         <div className="form-group">
           <label className="mb-2" for="password">
             Password
           </label>
-          <input onChange={(e) => setLoginPassword(e.target.value)} id="password" type="password" class="form-control" />
+          <input
+            onChange={(e) => setLoginPassword(e.target.value)}
+            id="password"
+            type="password"
+            class="form-control"
+          />
         </div>
+        <p>
+          Don't have an account?{" "}
+          <Link
+            className="underline text-primary hover:cursor-pointer"
+            to={"/signUp"}
+          >
+            Sign Up
+          </Link>
+        </p>
         <div className="form-group">
-          <button className="btn btn-info">Submit</button>
+          <button className="btn bg-primary text-white hover:bg-primary_hover hover:cursor-pointer">
+            Submit
+          </button>
         </div>
       </form>
     </div>
