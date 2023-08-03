731139943fbbd0d2e6408404f60496aaeb63983e
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 20:39:40 +0530

    chaging credentils

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cors(
     //   }
     // },
     origin: "http://localhost:3000",
-    credentials: true,
+    // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
 ));
