7a93012da9a780f7a6b3f401c4279b0fd97b0d18
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-03 19:51:26 +0530

    setting origin to frontend

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cors(
     //     callback(new Error('Not allowed by CORS'))
     //   }
     // },
-    origin: "*",
+    origin: "https://vercel.com/maxassassins/feelance-me-frontend",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
