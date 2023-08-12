28d95c270a6c1271cd368b5f9d21e4ecc104341b
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-12 23:42:43 +0530

    footer is improved

--- client/src/component/Footer/Footer.js
+++ client/src/component/Footer/Footer.js
@@ -1,18 +1,30 @@
 import React from "react";
-import "./footer.css";
 import "../../component/common.css";
 import { BsFacebook, BsInstagram } from "react-icons/bs";
 import { FaGooglePlay } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="global-footer">
-      <div className="socials">
-        <BsFacebook />
-        <BsInstagram />
-        <FaGooglePlay />
+    <footer className="px-6 py-6 text-light_heading border-t w-full">
+      <div className="flex items-center gap-12 justify-between">
+        <div className="flex gap-2">
+          <h4 className="font-[Pacifico,cursive] font-semibold text-2xl">
+            FreelanceMe
+          </h4>
+        </div>
+        <div className="text-xl  flex items-center gap-4">
+          <Link className="p-2 hover:bg-dark_separator rounded-full">
+            <BsFacebook className="" />
+          </Link>
+          <Link className="p-2 hover:bg-dark_separator rounded-full">
+            <BsInstagram />
+          </Link>
+          <Link className="p-2 hover:bg-dark_separator rounded-full">
+            <FaGooglePlay />
+          </Link>
+        </div>
       </div>
-      <div>Created by Mohd. Shadab</div>
     </footer>
   );
 };
