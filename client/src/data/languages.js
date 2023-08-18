5d922e831eee0f0be04ff9100fa9fd2baa1f4d2f
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-08-18 17:53:17 +0530

    added languages list with expretise levels

---
+++ client/src/data/languages.js
@@ -0,0 +1,29 @@
+export const languagesData = [
+  "English",
+  "Mandarin Chinese",
+  "Hindi",
+  "Spanish",
+  "French",
+  "Arabic",
+  "Bengali",
+  "Russian",
+  "Portuguese",
+  "Urdu",
+  "Indonesian",
+  "German",
+  "Japanese",
+  "Swahili",
+  "Punjabi",
+  "Korean",
+  "Turkish",
+  "Italian",
+  "Dutch",
+  "Tamil",
+];
+
+export const languageFluencyLevelData = [
+  "basic",
+  "conversational",
+  "fluent",
+  "native/bilingual",
+];
