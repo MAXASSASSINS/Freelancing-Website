59efcc609374fe5729f63d4a767225147aa7e6e8
MAXASSASSINS <mohammedshadab23april@gmail.com>
2024-10-03 20:54:39 +0530

    add some global constants

--- client/src/constants/globalConstants.ts
+++ client/src/constants/globalConstants.ts
@@ -3,3 +3,7 @@ export const MULTIPLE_CHOICE = "multiple choice";
 export type QuestionType = typeof FREE_TEXT | typeof MULTIPLE_CHOICE;
 
 export const CHOOSE_A_DELIVERY_TIME = "choose a delivery time";
+
+export const KB = 1024;
+export const MB = 1024 * 1024;
+export const GB = 1024 * 1024 * 1024;
