55782841dd437ee7e7fea588cae05871465be5ed
MAXASSASSINS <mohammedshadab23april@gmail.com>
2024-10-10 17:35:45 +0530

    fix socket url

--- client/src/utility/axiosInstance.ts
+++ client/src/utility/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL =
+export const baseURL =
   process.env.REACT_APP_MODE === "PROD"
     ? "https://freelanceme-backend.onrender.com" // Production URL
     : "http://localhost:4000"; // Development URL
