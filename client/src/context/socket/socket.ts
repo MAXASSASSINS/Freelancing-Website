529f272446cd97f70a184a919c1f33c77df68291
MAXASSASSINS <mohammedshadab23april@gmail.com>
2024-10-10 17:36:18 +0530

    add auth token to sokcet connection

--- client/src/context/socket/socket.ts
+++ client/src/context/socket/socket.ts
@@ -1,7 +1,13 @@
 import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
+import Cookies from "js-cookie";
+import { baseURL } from "../../utility/axiosInstance";
 
 // export const socket = io("");
-export const socket = io("http://localhost:4000");
+export const socket = io(baseURL, {
+  auth: {
+    token: Cookies.get("token"),
+  }
+});
 // export const socket = io("https://freelanceme-backend.onrender.com/");
 export const SocketContext = createContext<Socket>(socket);
